a65b9c3c70ac5793928f8daaf364b286958c1ae9
Hossein Tabasi <husseintabasi8@gmail.com>
2026-09-02 06:59:36 -0700

    Remove misplaced Core files from src root

--- src/Notebook.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartKnowledgeHub.Core.Models;
-
-public sealed class Notebook
-{
-    public long Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public DateTime CreatedUtc { get; set; }
-    public int NoteCount { get; set; }
-}
