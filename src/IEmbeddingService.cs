bbca063e99b8c1db511c92977a29e1c9b24c63fd
Hossein Tabasi <husseintabasi8@gmail.com>
2026-09-02 06:45:54 -0700

    Remove misplaced Core files from src root

--- src/IEmbeddingService.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartKnowledgeHub.Core.Embedding;
-
-public interface IEmbeddingService
-{
-    string Name { get; }
-    bool IsAvailable { get; }
-    int Dimensions { get; }
-    float[] Embed(string text);
-}
